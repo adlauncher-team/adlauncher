ea104fe7622b6ce086ee12d81f8a2f40d051a548
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-04-11 17:32:18 -0600

    Update script.js

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,17 +1,19 @@
 addEventListener('DOMContentLoaded', () => {
-  const $ = selector => document.querySelector(selector);
-  const $$ = selector => document.querySelectorAll(selector);
+  // CONSTANTES - VARIABLES
+  const $ = (selector) => document.querySelector(selector);
 
-  const $play = $('#play');
+  // BARRA DE OPCIONES
+  const $changelogs = $('#changelogs');
   const $settings = $('#settings');
-  const $versionSelector = $('.version-selector'); 
-  const $versions = $('.versions');
+  const $versionsPage = $('#versions');
+  const $play = $('#play');
+
+  const $versionSelector = $('.version-selector');
   const $versionsIcon = $('#version-selector');
   const $versionText = $('#version-text');
+  const $versions = $('.versions');
   const $userText = $('#profile-user');
-  const $changelogs = $('#changelogs');
   const $logo = $('#img');
-  const $downloadBar = $('.download-bar');
 
   let version, user;
 
@@ -22,17 +24,24 @@ addEventListener('DOMContentLoaded', () => {
   $changelogs.addEventListener('click', () => {
     document.location.href = 'pages/changelogs.html';
   });
-  
+
+  $versionsPage.addEventListener('click', () => {
+    document.location.href = 'pages/versions.html';
+  });
+
+  // FUNCION PARA EJECUTAR EL JUEGO CON EL USUARIO Y LA VERSION INDICADA
   $play.addEventListener('click', () => {
     user = $userText.textContent;
     version = $versionText.textContent;
-    if(!version || version === 'Descargar Version' || version === 'Select a Version' || !user) {
+    if (!version || version === 'Select a Version' || !user) {
       alert('Select a version and user');
     } else {
+      $('.playing-container').classList.remove('hidden');
       window.adlauncher.play(user, version);
     }
   });
-  
+
+  // FUNCION PARA DESPLEGAR LA PESTAÑA DE VERSIONES
   $versionSelector.addEventListener('click', () => {
     window.adlauncher.getVersions();
     $versionsIcon.classList.toggle('rotate');
@@ -40,45 +49,22 @@ addEventListener('DOMContentLoaded', () => {
     $versions.style.overflow = 'auto';
   });
 
-  document.addEventListener('click', () => {
-    if(!$versions.classList.contains('hidden')) {
-      setTimeout(() => {
-        versionpar();
-      }, 10);
-    };
+  // FUNCION PARA SELECCIONAR UNA VERSION
+  $versions.addEventListener('click', (e) => {
+    const element = e.target;
+    if (window.innerHeight - $versions.getBoundingClientRect().bottom < 38) {
+      $versions.style.bottom = '5vh';
+    }
+    if (element.classList.contains('version')) {
+      version = element.textContent;
+      $versionText.innerText = version;
+      window.adlauncher.getImg(version);
+      $versions.classList.toggle('hidden');
+      $versionsIcon.classList.toggle('rotate');
+      let vImg = Math.floor(version.split('.')[1]);
+      if (!isNaN(vImg) && vImg !== 10 && !version.includes('fabric')) {
+        $logo.src = `assets/minecraft-1.${vImg}.jpg`;
+      }
+    }
   });
-
-  function manejarClic() {
-    version = this.textContent;
-    $versionText.innerText = version;
-    window.adlauncher.getImg(version);
-    $versions.classList.toggle('hidden');
-    $versionsIcon.classList.toggle('rotate');
-    this.removeEventListener('click', manejarClic);
-    let vImg = Math.floor(version.split('.')[1]);
-    if(!isNaN(vImg) && vImg !== 10 && !version.includes('fabric')) {
-      $logo.src = `assets/minecraft-1.${vImg}.jpg`;
-    };
-  };
-
-  function plpl() {
-    if($downloadBar.classList.contains('hidden')) {
-      window.adlauncher.input('version');
-      this.removeEventListener('click', plpl);
-    } else {
-      alert('Ya se está descargando una versión...')
-    }
-  }
-
-  function versionpar() {
-    if((window.innerHeight - $versions.getBoundingClientRect().bottom) < 38) {
-      $versions.style.bottom = '5vh'
-    }
-    const $version = $$('.version');
-    const $downloadVersion = $('#download-version');
-    $downloadVersion.addEventListener('click', plpl);
-    $version.forEach(element => {
-      element.addEventListener("click", manejarClic);
-    });
-  }
 });
